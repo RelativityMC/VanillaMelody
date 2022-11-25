b15ee2ec3c0ed285e03714e2b0eb83b84172e43e
ishland <ishlandmc@yeah.net>
2022-11-25 09:26:01 +0800

    Improvements

--- src/main/java/com/ishland/vanillamelody/VanillaMelodyMod.java
+++ src/main/java/com/ishland/vanillamelody/VanillaMelodyMod.java
@@ -1,9 +1,11 @@
 package com.ishland.vanillamelody;
 
+import com.ishland.vanillamelody.common.Config;
 import net.fabricmc.api.ModInitializer;
 
 public class VanillaMelodyMod implements ModInitializer {
     @Override
     public void onInitialize() {
+        Config.init();
     }
 }
